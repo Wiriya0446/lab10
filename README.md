488f4d3b82fc4610a80b21398ea5cf82d268a692
Wiriya0446 <wiriyakhotchahin@gmail.com>
2023-12-22 15:18:36 +0700

    finish

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# NAME:
-# STUDENT ID:
+# NAME:Wiriya Kohtchahin
+# STUDENT ID:660610846
--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    ifstream source;
+    string score;
+    int i=0;
+    double Mean,deviation,sum;
+    source.open("score.txt");
+    while(getline(source,score)){
+        sum = sum +stod(score);
+        i++;
+    }
+    Mean=sum/i;
+    sum=0;
+    source.close();
+    source.open("score.txt");
+    while(getline(source,score)){
+        sum = sum + pow(stod(score),2);
+    }
+    deviation=pow((sum/i)-pow(Mean,2),0.5);
+    cout << "Number of data = "<<i<<"\n";
+    cout << setprecision(3);
+    cout << "Mean = "<<Mean<<"\n";
+    cout << "Standard deviation = "<<deviation<<"\n";
+}
